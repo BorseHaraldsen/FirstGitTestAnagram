ed17da1145e13dda42e3accf36781968bf7dcb89
Maiwand Mohamand <maiwand@Maiwands-MacBook-Air.local>
2023-09-04 15:43:34 +0200

    update 3

--- src/main/java/org/example/AnagramGeneratorFour.java
+++ src/main/java/org/example/AnagramGeneratorFour.java
@@ -26,7 +26,7 @@ public class AnagramGeneratorFour {
 
         boolean result2 = true;
 
-        //heisann hoppsann shdsjhd s
+        //heisann
 
         if (s1.length() != s3.length()) {
             result2 = false;
